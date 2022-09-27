40dd9d536c8b7758d8f538c37ca194606629bade
Vitor Pereira <vitor-pcruz@hotmail.com>
2022-09-27 14:51:45 -0300

    Adicionando mappings em DevIO.Data

---
+++ src/DevIO.Data/Mappings/ProductMapping.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DevIO.Data.Mappings;
+
+public class ProductMapping : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasKey(product => product.Id);
+
+        builder.Property(product => product.Name)
+            .IsRequired()
+            .HasColumnType("varchar(200)");
+
+        builder.Property(product => product.Description)
+            .IsRequired()
+            .HasColumnType("varchar(1000)");
+
+        builder.Property(product => product.Image)
+            .IsRequired()
+            .HasColumnType("varchar(100)");
+
+        builder.ToTable("Products");
+    }
+}
---
+++ src/DevIO.Data/Mappings/SupplierMapping.cs
@@ -0,0 +1,32 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DevIO.Data.Mappings;
+
+public class SupplierMapping : IEntityTypeConfiguration<Supplier>
+{
+    public void Configure(EntityTypeBuilder<Supplier> builder)
+    {
+        builder.HasKey(supplier => supplier.Id);
+
+        builder.Property(supplier => supplier.Name)
+            .IsRequired()
+            .HasColumnType("varchar(200)");
+
+        builder.Property(supplier => supplier.Document)
+            .IsRequired()
+            .HasColumnType("varchar(14)");
+
+        // 1:1 => Supplier > Address
+        builder.HasOne(supplier => supplier.Address)
+            .WithOne(address => address.Supplier);
+
+        // 1:N => Supplier > Products
+        builder.HasMany(supplier => supplier.Products)
+            .WithOne(product => product.Supplier)
+            .HasForeignKey(product => product.SupplierId);
+
+        builder.ToTable("Suppliers");
+    }
+}
