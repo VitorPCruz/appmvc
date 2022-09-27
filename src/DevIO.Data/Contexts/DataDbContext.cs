3fc5cd828e2b326ab1da4dff6db7a178598229dc
Vitor Pereira <vitor-pcruz@hotmail.com>
2022-09-27 11:55:35 -0300

    Adicionando dbsets em context de data

---
+++ src/DevIO.Data/Contexts/DataDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Contexts;
+
+public class DataDbContext : DbContext
+{
+	public DataDbContext(DbContextOptions options) : base(options) { }
+
+	public DbSet<Product> Products { get; set; }
+    public DbSet<Supplier> Suppliers { get; set; }
+    public DbSet<Address> Addresses { get; set; }
+}
